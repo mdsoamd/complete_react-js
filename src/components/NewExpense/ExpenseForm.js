2e6106a1d9207e1d1e2a88096b67eeda3641d1f7
Md Somad <mdsomad10@gmail.com>
2023-04-02 15:21:46 +0530

    complete

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,29 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+
+const [ enteredTit1e , setEnteredTit1e ] = useState('')
+const [ enteredAmount , setEnteredAmount ] = useState('')
+const [ enteredDate ,  setEnteredDate ] = useState('')
+
+
+//TODO Create TitleChangeHandler Finction
+const titleChangeHandler = (event)=>{
+   setEnteredTit1e(event.target.value)
+}
+
+//TODO Create amountChangeHandler Finction
+const amountChangeHandler = (event)=>{
+    setEnteredAmount(event.target.value)
+}
+
+//TODO Create dateChangeHandler Finction
+const dateChangeHandler = (event)=>{
+    setEnteredDate(event.target.value)
+}
+
+
+
+//TODO Create dateChangeHandler Finction
+const submitHandler = (event)=>{
+    event.preventDefault();
+
+    const expenseData = {
+        title: enteredTit1e,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    }
+     
+    setEnteredTit1e('')
+    setEnteredAmount('')
+    setEnteredDate('')
+    console.log(expenseData);
+    
+    
+}
+
+
+    
+    
   return (
-    <form>
+    <form onSubmit={submitHandler}>
+
       <div className="new-expense__controls">
 
         <div className="new-expense__control">
-            
+
           <label>Title</label>
-          <input type="text" />
+          <input type="text" value={enteredTit1e} onChange={titleChangeHandler}/>
 
         </div>
 
         <div className="new-expense__control">
 
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
 
         </div>
 
         <div className="new-expense__control">
 
           <label>Date</label>
-          <input type="date" />
+          <input type="date" value={enteredDate} onChange={dateChangeHandler}/>
 
         </div>
 
