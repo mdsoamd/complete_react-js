9f330dd6e85d8d5ef1b81cf5fbde9a936399feb3
Md Somad <mdsomad10@gmail.com>
2023-04-02 11:50:25 +0530

    complete

--- src/components/Expense/Expenseltem.js
+++ src/components/Expense/Expenseltem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
@@ -6,7 +6,22 @@ import Card from '../UI/Card'
 
 //TODO This is Component
 const ExpenseItem = (props) => {
+
+   const [newTitle,setNewTitle] = useState('')      //*  <-- by default value Ek Dena padta hai
+
+   const [title,setTitle] = useState(props.title)   //*  <-- by default value Ek Dena padta hai
   
+    const clickHandler = () => {
+       setTitle(newTitle)
+    }
+
+
+    const changeHandler = (event) => {
+       setNewTitle(event.target.value)     //* <-- input field Value get event
+    }
+
+
+
     
     
     return(
@@ -15,9 +30,15 @@ const ExpenseItem = (props) => {
           <ExpenseDate date={props.date}/>
 
             <div className='expens-item__description'>
-                <h2>{props.title}</h2>
+                
+                <h2>{title}</h2>
+
                 <div className='expens-item__price'>${props.amount}</div>
             </div>
+
+            <input type="text" value={newTitle} onChange={changeHandler}/>
+
+            <button onClick={clickHandler}>Change Title</button>
             
         </Card>
     )
